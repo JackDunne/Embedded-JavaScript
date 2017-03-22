385fff4616bc2692117462cbce60c7689499f480
JackDunne <8jdunne@stpatrick.org>
2017-03-22 11:25:40 -0500

    finished the first challenge

--- click.js
+++ click.js
@@ -1,18 +1,26 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
+  let color = "#993300";
   const doc = document;
   let alldivs = doc.querySelectorAll("#samecolor div");
   for(let x = 0;x<alldivs.length;x++){
-    alldivs[x].style.background-color = hex;
-    alldivs[x].style.opacity = Math.Random();
+    alldivs[x].style.backgroundColor = color;
+    alldivs[x].style.opacity = Math.random();
   }
 }
 
 //declare diffcolor() here
-function diffcolor(hex){
+function diffcolor(){
+  let firstcolor = "#999900";
+  let secondcolor = "#990099";
   const doc = document;
-  let finddivs = doc.getElementById("diffcolor > div");
+    let finddivs = doc.querySelectorAll("#diffcolor div");
   for(let i = 0;i<finddivs.length;i++){
-
+    if(i%2 == 0){
+      finddivs[i].style.backgroundColor = firstcolor;
+    }
+    else{
+      finddivs[i].style.backgroundColor = secondcolor;
+    }
   }
 }
