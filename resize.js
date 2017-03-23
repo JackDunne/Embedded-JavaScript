f816f3ab8a002975bb45924af201a01fe90df7f7
JackDunne <8jdunne@stpatrick.org>
2017-03-23 10:49:19 -0500

    started the resize challenge

--- resize.js
+++ resize.js
@@ -1,4 +1,22 @@
 //Declare tabletSize() here
-
-
+function tabletSize(){
+  const doc = document;
+  let height = "800px*0.75";
+  let width = "1440px*0.75";
+  let graballdivs = doc.querySelectorAll("section");
+  for(let x = 0;x<graballdivs.length;x++){
+    graballdivs[x].style.height = height;
+    graballdivs[x].style.width = width;
+  }
+}
 //Declare phoneSize() here
+function phoneSize(){
+  const doc = document;
+  let height = "800px*0.5";
+  let width = "1440px*0.5";
+  let graballdivs = doc.querySelectorAll("section");
+  for(let i = 0;i<graballdivs.length;i++){
+    graballdivs[i].style.height = height;
+    graballdivs[i].style.width = width;
+  }
+}
